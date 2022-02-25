bf2b3f62cfb863c716fae220b22819c24a614701
Yash Parwani <parwaniyash5610@gmail.com>
2022-02-25 20:47:32 +0530

    creating schema for posts

---
+++ models/posts.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema({
+    content:{
+        type: String,
+        required: true
+    },
+    //linking postschema to userschema so that we can link user who has commented
+    user:{
+        type : mongoose.Schema.Types.ObjectId,
+        //type : mongoose.SchemaTypes.ObjectId
+        ref : 'user'
+    }
+},{
+        timestamps:true
+    }
+);
+
+
+const Post = mongoose.model("Post",postSchema);
+
+module.exports = Post;
