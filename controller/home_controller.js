a5dfaa979d2335b2a9df788695eaaeb892425246
Yash Parwani <parwaniyash5610@gmail.com>
2022-02-28 22:10:09 +0530

    Nested Population :: displaying comments and related user

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,10 +1,18 @@
 const Post = require("../models/posts");
 
 
+
 module.exports.home = function(request,response){
     //rendering home.ejs and sending title from server to browser
     //cookies come as in a request so to check we print it as request.cookies
-    Post.find({}).populate('user').exec(function(error,posts){
+    Post.find({}).populate('user')
+    .populate({
+        path:"comments",
+        populate: ({
+            path:"user"
+        })
+    })
+    .exec(function(error,posts){
         if(error){
             console.log("Error in fectching posts to display on homescreen");
             console.log("Error message is : ",error);
