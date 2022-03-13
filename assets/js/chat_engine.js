61400a0a72fcbbb4799e9bc5863c36dc2571b192
Yash Parwani <parwaniyash5610@gmail.com>
2022-03-13 11:37:41 +0530

    setting up socket.io

---
+++ assets/js/chat_engine.js
@@ -0,0 +1,1 @@
+//this file will be handling sockets on the front-end i.e client side i.e of the subscribers of our observer-subscriber pattern
---
+++ config/chat_sockets.js
@@ -0,0 +1,5 @@
+//this will be handling sockets on the backend i.e its the observer (server) of our observer-subscriber pattern
+
+module.exports.chatSockets = function(socketServer){
+    //socketServer is the chatServer that is sent from the main index.js using above chatSockets function only
+}
